94a55de88f0ce5c2d6db67a920c2f82e92cbabd5
Brent Sullivan <brntsllvn@gmail.com>
2016-11-05 14:25:06 -0700

    refactor to use nested pattern matching

--- src/main/scala/com/persist/uw/examples/Simplify.scala
+++ src/main/scala/com/persist/uw/examples/Simplify.scala
@@ -8,22 +8,23 @@ object Simplify {
 
   def simplify(t: Tree3): Tree3 = {
     t match {
-      case Int3(i)                            => Int3(i)
-      case Add3(left: Int3, right: Int3)     => Int3(left.i + right.i)
-      case Add3(left, right)                  => simplify(Add3(simplify(left), simplify(right)))
-      case Subtract3(left: Int3, right: Int3) => Int3(left.i - right.i)
-      case Subtract3(left, right)             => simplify(Subtract3(simplify(left), simplify(right)))
-      case Multiply3(left: Int3, right: Int3) => Int3(left.i * right.i)
-      case Multiply3(left, right)             => simplify(Multiply3(simplify(left), simplify(right)))
-      case Divide3(left: Int3, right: Int3)   => Int3(left.i / right.i)
-      case Divide3(left, right)               => simplify(Divide3(simplify(left), simplify(right)))
-
-      case Add3(left: Int3,  right: Name3) =>
-        if(left.i == 0) right
-        else right // not correct, but helps "+0" tests pass
-      case Add3(left: Name3, right: Int3)  =>
-        if(right.i == 0) left
-        else left // not correct, but helps "+0" tests pass
+      case Int3(i) => Int3(i)
+      case Add3(left, right) => (left, right) match {
+        case (Int3(i), Int3(j)) => Int3(i + j)
+        case (_, _) => simplify(Add3(simplify(left), simplify(right)))
+      }
+      case Subtract3(left, right) => (left, right) match {
+        case (Int3(i), Int3(j)) => Int3(i - j)
+        case (_, _) => simplify(Subtract3(simplify(left), simplify(right)))
+      }
+      case Multiply3(left, right) => (left, right) match {
+        case (Int3(i), Int3(j)) => Int3(i * j)
+        case (_, _) => simplify(Multiply3(simplify(left), simplify(right)))
+      }
+      case Divide3(left, right) => (left, right) match {
+        case (Int3(i), Int3(j)) => Int3(i / j)
+        case (_, _) => simplify(Divide3(simplify(left), simplify(right)))
+      }
     }
   }
 }
